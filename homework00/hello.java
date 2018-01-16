1d917adcf7f56f138a1b9bbb834f3b400044fccc
avasqu27 <35350821+avasqu27@users.noreply.github.com>
2018-01-15 23:46:28 -0800

    Update hello.java

--- homework00/hello.java
+++ homework00/hello.java
@@ -9,7 +9,7 @@
  * Exceptions  : None
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 public class HelloWorld {
-   public static void main( String[] args ) {
+   public static void main( String args[] ) {
       System.out.println("\n Hello, World!" );
    }
 }
