e6c326db265b760a5f8fc1a36e54aefe5dd24b07
avasqu27 <35350821+avasqu27@users.noreply.github.com>
2018-01-15 21:55:22 -0800

    Update hello.java

--- homework00/hello.java
+++ homework00/hello.java
@@ -1,5 +1,17 @@
-public class HelloWorld {
-   public static void main("String[] args) {
-      System.out.pringIn("Hello, world!");
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * File name   : hello.java
+ * Purpose     : Displays "Hellow World!"
+ * Author      : Alejandra Vasquez 
+ * Date        : 01/15/2018
+ * Description : None
+ * Notes       : None
+ * Warnings    : None
+ * Exceptions  : None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+public class HelloWorld{
+   public static void main( String args[] ) {
+      System.out.println("Hellow, world!");
    }
+
 }
